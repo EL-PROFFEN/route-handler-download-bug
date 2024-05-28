ced9e8d18d372b1c428341af4d86b2814ad692ca
Philip Rothschild <philiprene89@gmail.com>
2024-05-28 15:47:08 +0200

    Adding a download button using a native link element

--- app/components/download-buttons-rsc.tsx
+++ app/components/download-buttons-rsc.tsx
@@ -1,11 +1,24 @@
 import { Button } from "@/components/ui/button";
 import { Download } from "lucide-react";
+import Link from "next/link";
 
 export function DownloadButtonNativeLinkRSC() {
   return (
-    <Button size="sm" className="flex gap-2 border-dashed">
-      <a href="/api/download">Download with native link</a>
-      <Download className="h-4 w-4" />
+    <a
+      href="/api/download"
+      className="flex gap-2 border-dashed border p-2 items-center"
+    >
+      Download using native link (RSC) <Download className="h-4 w-4" />
+    </a>
+  );
+}
+
+export function DownloadButtonNextLinkRSC() {
+  return (
+    <Button asChild variant="outline">
+      <Link href="/api/download" className="flex gap-2">
+        Download using Next Link (RSC) <Download className="h-4 w-4" />
+      </Link>
     </Button>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
-import { DownloadButtonNativeLinkRSC } from "./components/download-buttons-rsc";
+import {
+  DownloadButtonNativeLinkRSC,
+  DownloadButtonNextLinkRSC,
+} from "./components/download-buttons-rsc";
 
 export default function Home() {
   return (
     <div className="space-y-4 m-4">
       <DownloadButtonNativeLinkRSC />
+      <DownloadButtonNextLinkRSC />
     </div>
   );
 }
